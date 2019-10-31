7ed426985f67255952cf5807afc54a98abb167d4
LutenkoMikhail <lamprologus2011@gmail.com>
2019-10-31 16:07:15 +0200

    Add migrations tables

---
+++ database/migrations/2019_10_31_132427_create_employees_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50)->index();
+            $table->string('surname', 50);
+            $table->date('birthday');
+            $table->string('position_employee', 50);
+            $table->float('payment',10,2);
+            $table->boolean('type_payment_employee');
+            $table->unsignedBigInteger('department_id');
+            $table->timestamps();
+
+        });
+        Schema::table('employees', function (Blueprint $table) {
+            $table->foreign('department_id')
+                ->references('id')
+                ->on('departments')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
