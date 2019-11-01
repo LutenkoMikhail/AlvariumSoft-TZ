d85e38ea9253a69f30bd96dc1ed08fa8d1f747ce
LutenkoMikhail <lamprologus2011@gmail.com>
2019-11-01 12:01:16 +0200

    Add controllers

--- app/Department.php
+++ app/Department.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'name_department'
+    ];
+
+    public function employee()
+    {
+        return $this->hasMany(\App\Employee::class);
+    }
 }
--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id','name', 'surname', 'birthday', 'position_employee', 'payment', 'type_payment_employee', 'department_id'
+    ];
+
+    public function department()
+    {
+        return $this->hasOne(\APP\Department::class);
+    }
 }
